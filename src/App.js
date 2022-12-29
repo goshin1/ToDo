78f796dfadf4b78ec8ff4657387fec4b5a1e7631
shin1 <gci787@naver.com>
2022-12-29 21:44:18 +0900

    html 작성 및 기본구조 작성

--- src/App.js
+++ src/App.js
@@ -10,20 +10,33 @@ function ToDoForm(props){
             event.preventDefault();
           }}/>
         </p>
-        <label for="todo"><input id="todo" type="text" name="todo" placeholder="To Do"/></label><br/>
-        <label for="detail" ><textarea id="detail" name="detail" placeholder="Detail content" cols="22" rows="5"></textarea></label><br/>
-        <label for="date">Date <input id="date" type="date"/></label><br/>
-        <label for="time">Time <input id="time" type="time"  /></label>
+        <label htmlFor="todo"><input id="todo" type="text" name="todo" placeholder="To Do"/></label><br/>
+        <label htmlFor="detail" ><textarea id="detail" name="detail" placeholder="Detail content" cols="22" rows="5"></textarea></label><br/>
+        <label htmlFor="date">Date <input id="date" type="date"/></label><br/>
+        <label htmlFor="time">Time <input id="time" type="time"  /></label>
       </form>
     </div>
   );
 }
 
+function List(props){
+  const lis = [];
+  const calenders = props.calenders;
+  for(let i = 0; i < calenders.length; i++){
+    lis.push(<ListBlock key={calenders[i].id} num={calenders[i].id} todo={calenders[i].todo} detail={calenders[i].detail} date={calenders[i].date} time={calenders[i].time}></ListBlock>)
+  }
+
+  return (
+    <div id='list'>
+      {lis}
+    </div>
+  );
+}
 
 function ListBlock(props){
   return (
-    <li>
-      <div>
+    <li key={props.num}>
+      <div className="listTop">
         <button onClick={()=>{
 
         }}></button>
@@ -32,7 +45,7 @@ function ListBlock(props){
           
         }}></button>
       </div>
-      <div>
+      <div className="listBottom">
         <button>{props.date}{props.time}</button>
         <p>{props.detail}</p>
       </div>
@@ -41,9 +54,15 @@ function ListBlock(props){
 }
 
 function App() {
+  const calenders = [
+    {id:1, todo : '해야 할 일', detail : '세부적인 내용', date : '2022-12-29', time : '오후 03 : 12'},
+    {id:2, todo : '해야 할 일', detail : '세부적인 내용', date : '2022-12-29', time : '오후 03 : 12'}
+  ];
+
   return (
     <div className="App">
       <ToDoForm></ToDoForm>
+      <List calenders={calenders}></List>
     </div>
   );
 }
